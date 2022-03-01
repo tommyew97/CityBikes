5e0d9aca111a22fd99c844c27c2d04d34e7cd25a
Maria Opland <mariaopl@stud.ntnu.no>
2022-03-01 15:49:24 +0100

    Implemented start page

---
+++ CityBikes/app/src/main/java/com/example/citybikes/ListView.java
@@ -0,0 +1,15 @@
+package com.example.citybikes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+    }
+}
