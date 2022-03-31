fc621c392c2452b607295283dd65c7316465a0e2
Juan Rios <77796276+Juanrios016@users.noreply.github.com>
2022-03-31 15:30:39 +0200

    Implemented Google Maps

--- CityBikes/app/src/main/java/com/example/citybikes/ui/map/MapFragment.java
+++ CityBikes/app/src/main/java/com/example/citybikes/ui/map/MapFragment.java
@@ -1,27 +1,30 @@
 package com.example.citybikes.ui.map;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.example.citybikes.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * CLass that creates a fragment for the 'Map' section. It handles the
  * visualization and renders necessary elements
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements  OnMapReadyCallback  {
 
     private MapViewModel mViewModel;
-
     public static MapFragment newInstance() {
         return new MapFragment();
     }
@@ -29,7 +32,14 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.map_fragment, container, false);
+        View view =  inflater.inflate(R.layout.map_fragment, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+//        ------------------------------------
+
+        return view;
     }
 
     @Override
@@ -37,6 +47,15 @@ public class MapFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MapViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 }
