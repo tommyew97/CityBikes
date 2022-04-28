4ea0da6f64c1dec870e2a5509976f83f411af0cd
Juan Rios <77796276+Juanrios016@users.noreply.github.com>
2022-04-28 16:47:34 +0200

    Cleaned code

--- CityBikes/app/src/main/java/com/example/citybikes/ui/map/MapFragment.java
+++ CityBikes/app/src/main/java/com/example/citybikes/ui/map/MapFragment.java
@@ -9,16 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.citybikes.MainActivity;
 import com.example.citybikes.R;
-import com.example.citybikes.ui.list.ListFragment;
 import com.example.citybikes.util.CalculateDistance;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
